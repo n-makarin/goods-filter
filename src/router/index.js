7b786f4fc80bafaba3c9a454c12666865dfe9b77
Nikita Makarin <n-makarin@mail.ru>
2019-12-20 14:22:35 +0300

    Add /goods path to router/indes.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,13 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    redirect: '/goods'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/goods',
+    name: 'goods',
+    component: () => import('../views/Goods.vue')
   }
 ]
 
