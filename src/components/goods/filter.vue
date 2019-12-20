19cd26ecc2ffc4f0645dc9f5ed25e0124159152a
Nikita Makarin <n-makarin@mail.ru>
2019-12-20 18:21:07 +0300

    Change markup and styles in filter.vue

--- src/components/goods/filter.vue
+++ src/components/goods/filter.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="filter">
-    <multiselect
-      v-model="categoryValue"
-      :options="categoryOptions"
-      :multiple="true"
-      :close-on-select="false"
-      :clear-on-select="false"
-      :preserve-search="true"
-      placeholder="Pick category"
-      label="name"
-      track-by="name"
-      class="filter__category"
-    />
-    <multiselect
-      v-model="brandValue"
-      :options="brandOptions"
-      :multiple="true"
-      :close-on-select="false"
-      :clear-on-select="false"
-      :preserve-search="true"
-      placeholder="Pick brand"
-      label="name"
-      track-by="name"
-      class="filter__brand"
-    />
+    <div class="filter__item">
+      <label for="category">Категории: </label>
+      <multiselect
+        id="category"
+        v-model="categoryValue"
+        :options="categoryOptions"
+        :multiple="true"
+        :close-on-select="false"
+        :clear-on-select="false"
+        :preserve-search="true"
+        placeholder="Выберите категории"
+        label="name"
+        track-by="name"
+        class="filter__category"
+      />
+    </div>
+    <div class="filter__item">
+      <label for="brands">Бренды: </label>
+      <multiselect
+        id="brands"
+        v-model="brandValue"
+        :options="brandOptions"
+        :multiple="true"
+        :close-on-select="false"
+        :clear-on-select="false"
+        :preserve-search="true"
+        placeholder="Выберите бренды"
+        label="name"
+        track-by="name"
+        class="filter__brand"
+      />
+    </div>
   </div>
 </template>
 
@@ -63,8 +71,19 @@ export default {
 <style lang="scss">
 .filter {
   display: flex;
+  height: 51px;
   .multiselect {
     @include vue-multiselect;
+    width: 100%;
+  }
+  &__item {
+    display: flex;
+    align-items: flex-start;
+    flex-basis: 400px;
+    label {
+      margin-right: 10px;
+      margin-top: 8px;
+    }
     &:first-child {
       margin-right: 20px;
     }
