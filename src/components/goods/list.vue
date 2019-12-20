a04a0d21c0619a61edc25812e9b914697a8d3b9a
Nikita Makarin <n-makarin@mail.ru>
2019-12-20 15:41:27 +0300

    Create src\components\goods\list.vue

---
+++ src/components/goods/list.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="list">
+    <item
+      v-for="item in data"
+      :key="item.id"
+      :name="item.name"
+      :category="item.category"
+      :brand="item.brand"
+      class="list__item"
+    />
+  </div>
+</template>
+
+<script>
+import Item from '@/components/goods/list/__item'
+
+export default {
+  components: {
+    Item
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  &__item {
+    width: 20%;
+    margin: 20px;
+  }
+}
+</style>
