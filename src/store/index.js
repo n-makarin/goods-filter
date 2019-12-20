5f826f51d1c43f42ad3d2882e51bc9f074c4fedc
Nikita Makarin <n-makarin@mail.ru>
2019-12-20 14:53:18 +0300

    Add goods module to src\store\index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import goods from '@/store/goods'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  namespaced: true,
   state: {
   },
   mutations: {
@@ -11,5 +14,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    goods
   }
 })
