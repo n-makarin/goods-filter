15e45d844884db10d4a0047772bb1c0083a5685d
Nikita Makarin <n-makarin@mail.ru>
2019-12-20 15:41:19 +0300

    Create src\components\goods\list\__item.vue

---
+++ src/components/goods/list/__item.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="item">
+    <div class="item__name">{{ name }}</div>
+    <div class="item__category">{{ category }}</div>
+    <div class="item__brand">{{ brand }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      default: 0
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    category: {
+      type: String,
+      default: ''
+    },
+    brand: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border: 1px solid rgb(196, 196, 196);
+  border-radius: 3px;
+  & > div {
+    margin-bottom: 10px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
