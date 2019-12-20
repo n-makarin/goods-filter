e3b00716db79a039ad21998f7b1558560453bdd7
Nikita Makarin <n-makarin@mail.ru>
2019-12-20 14:54:09 +0300

    Add 'goods' computed prop, getGoods method src\views\Goods.vue

--- src/views/Goods.vue
+++ src/views/Goods.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="goods">
-    goods
+    <h1>Список товаров</h1>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   components: {
+  },
+  computed: {
+    ...mapGetters({
+      goods: 'goods/list/data'
+    })
+  },
+  methods: {
+    ...mapActions({
+      getGoods: 'goods/list/get'
+    })
+  },
+  async mounted () {
+    await this.getGoods()
   }
 }
 </script>
