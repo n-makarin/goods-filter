96ded5ac3dc3b85afb809feb795a64268a7338ce
Nikita Makarin <n-makarin@mail.ru>
2019-12-20 18:07:43 +0300

    Create src\components\goods\filter.vue

---
+++ src/components/goods/filter.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="filter">
+    <multiselect
+      v-model="categoryValue"
+      :options="categoryOptions"
+      :multiple="true"
+      :close-on-select="false"
+      :clear-on-select="false"
+      :preserve-search="true"
+      placeholder="Pick category"
+      label="name"
+      track-by="name"
+      class="filter__category"
+    />
+    <multiselect
+      v-model="brandValue"
+      :options="brandOptions"
+      :multiple="true"
+      :close-on-select="false"
+      :clear-on-select="false"
+      :preserve-search="true"
+      placeholder="Pick brand"
+      label="name"
+      track-by="name"
+      class="filter__brand"
+    />
+  </div>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    Multiselect
+  },
+  data () {
+    return {
+      categoryValue: '',
+      brandValue: '',
+      categoryOptions: [
+        { name: 'Phone' },
+        { name: 'Tablet' },
+        { name: 'Notebook' }
+      ],
+      brandOptions: [
+        { name: 'Apple' },
+        { name: 'Samsung' },
+        { name: 'Xiaomi' },
+        { name: 'Oppo' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.filter {
+  display: flex;
+  .multiselect {
+    @include vue-multiselect;
+    &:first-child {
+      margin-right: 20px;
+    }
+  }
+}
+</style>
