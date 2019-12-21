32fd896eda07a76b93d54c0a7d007f5cba32480f
Nikita Makarin <n-makarin@mail.ru>
2019-12-21 13:53:14 +0300

    Remove excessive methods in src\views\Goods.vue

--- src/views/Goods.vue
+++ src/views/Goods.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="goods">
     <h1>Список товаров</h1>
-    <goods-filter class="goods__filter" />
+    <goods-filter class="goods__filter" @select="redirect" />
     <goods-list :data="goods" />
   </div>
 </template>
@@ -16,76 +16,21 @@ export default {
     GoodsList,
     GoodsFilter
   },
-  data () {
-    return {
-      availableQueryList: ['category', 'brand'],
-      queryNumber: 0
-    }
-  },
   computed: {
     ...mapGetters({
       goods: 'goods/list/data'
     }),
     query () {
-      return this.getGoodsQuery()
+      const path = this.$route.path
+      return this.$route.fullPath.replace(path, '')
     }
   },
   methods: {
     ...mapActions({
       getGoods: 'goods/list/get'
     }),
-    /**
-     * Get query by current url.
-     * It allows to make secure and based on availableQueryList property
-     * request to server avoiding to make it with any query
-     * @returns {string}
-     */
-    getGoodsQuery () {
-      const routeQuery = this.$route.query
-      let result = ''
-      this.availableQueryList.forEach((queryParam) => {
-        if (!routeQuery[queryParam]) { return }
-        result = result + this.getQueryString(routeQuery, queryParam)
-        this.queryNumber++
-      })
-      return result
-    },
-    /**
-     * Get stringified query
-     * @param {object} routeQuery
-     * @param {string} queryParam
-     * @returns {string}
-     */
-    getQueryString (routeQuery, queryParam) {
-      let result = ''
-      let query = ''
-      const queryValue = routeQuery[queryParam]
-      if (Array.isArray(queryValue)) {
-        query = this.getQueryWithMultipleParams(queryValue, queryParam)
-      } else {
-        query = `${queryParam}=${queryValue}`
-      }
-
-      if (this.queryNumber === 0) {
-        result = `?${query}`
-      } else {
-        result = `${result}&${query}`
-      }
-      return result
-    },
-    /**
-     * Get query with multiple params of some category
-     * @param {object} routeQuery
-     * @param {string} queryParam
-     * @returns {string}
-     */
-    getQueryWithMultipleParams (queryValue, queryParam) {
-      let query = ''
-      queryValue.forEach((element) => {
-        query = query ? `${query}&` : ''
-        query = `${query}${queryParam}=${element}`
-      })
-      return query
+    redirect (query) {
+      this.$router.push(`/goods${query}`)
     }
   },
   async mounted () {
